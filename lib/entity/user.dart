8c57cab06cee682406db57145700dcff81f84119
ywake <ywake@student.42tokyo.jp>
2021-09-04 19:09:01 +0900

    added other information to `User` class.

--- lib/entity/user.dart
+++ lib/entity/user.dart
@@ -8,7 +8,7 @@ class User {
   final int _id;
 
   /// user email address
-  final String _email;
+  final String? _email;
 
   /// user profile image
   final String _thumbnailImage;
@@ -18,6 +18,22 @@ class User {
 
   /// user name
   final String _screenName;
+
+  /// other information
+  final String _createdAt;
+  final bool _defaultProfile;
+  final bool _defaultProfileImage;
+  final String? _description;
+  final int _favouritesCount;
+  final int _followersCount;
+  final int _friendsCount;
+  final String _idStr;
+  final int _listedCount;
+  final String? _location;
+  final bool _protected;
+  final int _statusesCount;
+  final String? _url;
+  final bool _verified;
 
   /// The unique identifier of this user.
   ///
@@ -29,7 +45,7 @@ class User {
   /// If your Twitter account does not have an email address,
   /// or if the API is not configured to retrieve email addresses,
   /// you may not be able to retrieve email addresses.
-  String get email => _email;
+  String? get email => _email;
 
   /// thumbnailImage
   String get thumbnailImage => _thumbnailImage;
@@ -40,13 +56,72 @@ class User {
   /// Twitter account id
   String get screenName => _screenName;
 
+  /// The UTC datetime that the user account was created on Twitter. Not ISO 8601. e.g.
+  /// ```
+  /// "Sat May 09 17:58:22 +0000 2009"
+  /// ```
+  String get createdAt => _createdAt;
+
+  /// When true, indicates that the user has not altered the theme or background of their user profile.
+  bool get defaultProfile => _defaultProfile;
+
+  /// When true, indicates that the user has not uploaded their own profile image and a default image is used instead.
+  bool get defaultProfileImage => _defaultProfileImage;
+
+  /// The user-defined UTF-8 string describing their account.
+  String? get description => _description;
+
+  /// The number of Tweets this user has liked in the account’s lifetime. British spelling used in the field name for historical reasons.
+  int get favouritesCount => _favouritesCount;
+
+  /// The number of followers this account currently has. Under certain conditions of duress, this field will temporarily indicate “0”.
+  int get followersCount => _followersCount;
+
+  /// The number of users this account is following (AKA their “followings”). Under certain conditions of duress, this field will temporarily indicate “0”.
+  int get friendsCount => _friendsCount;
+
+  /// The string representation of the unique identifier for this User. Implementations should use this rather than the large, possibly un-consumable integer in id.
+  String get idStr => _idStr;
+
+  /// The number of public lists that this user is a member of.
+  int get listedCount => _listedCount;
+
+  /// The user-defined location for this account’s profile. Not necessarily a location, nor machine-parseable. This field will occasionally be fuzzily interpreted by the Search service.
+  String? get location => _location;
+
+  /// When true, indicates that this user has chosen to protect their Tweets.
+  bool get protected => _protected;
+
+  /// The number of Tweets (including retweets) issued by the user.
+  int get statusesCount => _statusesCount;
+
+  /// A URL provided by the user in association with their profile.
+  String? get url => _url;
+
+  /// When true, indicates that the user has a verified account. See [Verified Accounts](https://support.twitter.com/articles/119135-faqs-about-verified-accounts).
+  bool get verified => _verified;
+
   /// constructor
   User(Map<String, dynamic> params)
-      : this._id = params['id'] ?? '',
-        this._email = params['email'] ?? '',
+      : this._id = params['id'],
+        this._email = params['email'],
         this._thumbnailImage = params['profile_image_url_https'],
         this._name = params['name'],
-        this._screenName = params['screen_name'];
+        this._screenName = params['screen_name'],
+        this._createdAt = params['created_at'],
+        this._defaultProfile = params['default_profile'],
+        this._defaultProfileImage = params['default_profile_image'],
+        this._description = params['description'],
+        this._favouritesCount = params['favourites_count'],
+        this._followersCount = params['followers_count'],
+        this._friendsCount = params['friends_count'],
+        this._idStr = params['id_str'],
+        this._listedCount = params['listed_count'],
+        this._location = params['location'],
+        this._protected = params['protected'],
+        this._statusesCount = params['statuses_count'],
+        this._url = params['url'],
+        this._verified = params['verified'];
 
   /// get user info
   static Future<User> getUserData(
--- test/twitter_login_test.dart
+++ test/twitter_login_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:twitter_login/entity/user.dart';
 
@@ -9,27 +11,266 @@ void main() {
   /// you may not be able to retrieve email addresses.
   group('user response json parse test', () {
     test('parse', () {
-      final json = {
-        'email': 'test_email@test.com',
-        'profile_image_url_https': 'https://test.com/',
-        'name': 'test_name',
-        'screen_name': 'test_screen_name',
-      };
+      final resString = """
+      {
+        "contributors_enabled": true,
+        "created_at": "Sat May 09 17:58:22 +0000 2009",
+        "default_profile": false,
+        "default_profile_image": false,
+        "description": "I taught your phone that thing you like.  The Mobile Partner Engineer @Twitter. ",
+        "favourites_count": 588,
+        "follow_request_sent": null,
+        "followers_count": 10625,
+        "following": null,
+        "friends_count": 1181,
+        "geo_enabled": true,
+        "id": 38895958,
+        "id_str": "38895958",
+        "is_translator": false,
+        "lang": "en",
+        "listed_count": 190,
+        "location": "San Francisco",
+        "name": "Sean Cook",
+        "notifications": null,
+        "profile_background_color": "1A1B1F",
+        "profile_background_image_url": "http://a0.twimg.com/profile_background_images/495742332/purty_wood.png",
+        "profile_background_image_url_https": "https://si0.twimg.com/profile_background_images/495742332/purty_wood.png",
+        "profile_background_tile": true,
+        "profile_image_url": "http://a0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG",
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG",
+        "profile_link_color": "2FC2EF",
+        "profile_sidebar_border_color": "181A1E",
+        "profile_sidebar_fill_color": "252429",
+        "profile_text_color": "666666",
+        "profile_use_background_image": true,
+        "protected": false,
+        "screen_name": "theSeanCook",
+        "show_all_inline_media": true,
+        "status": {
+          "contributors": null,
+          "coordinates": {
+            "coordinates": [
+              -122.45037293,
+              37.76484123
+            ],
+            "type": "Point"
+          },
+          "created_at": "Tue Aug 28 05:44:24 +0000 2012",
+          "favorited": false,
+          "geo": {
+            "coordinates": [
+              37.76484123,
+              -122.45037293
+            ],
+            "type": "Point"
+          },
+          "id": 240323931419062272,
+          "id_str": "240323931419062272",
+          "in_reply_to_screen_name": "messl",
+          "in_reply_to_status_id": 240316959173009410,
+          "in_reply_to_status_id_str": "240316959173009410",
+          "in_reply_to_user_id": 18707866,
+          "in_reply_to_user_id_str": "18707866",
+          "place": {
+            "attributes": {},
+            "bounding_box": {
+              "coordinates": [
+                  [
+                    [
+                      -122.45778216,
+                      37.75932999
+                    ],
+                    [
+                      -122.44248216,
+                      37.75932999
+                    ],
+                    [
+                      -122.44248216,
+                      37.76752899
+                    ],
+                    [
+                      -122.45778216,
+                      37.76752899
+                    ]
+                  ]
+                ],
+                "type": "Polygon"
+              },
+              "country": "United States",
+              "country_code": "US",
+              "full_name": "Ashbury Heights, San Francisco",
+              "id": "866269c983527d5a",
+              "name": "Ashbury Heights",
+              "place_type": "neighborhood",
+              "url": "http://api.twitter.com/1/geo/id/866269c983527d5a.json"
+          },
+          "retweet_count": 0,
+          "retweeted": false,
+          "source": "Twitter for  iPhone",
+          "text": "@messl congrats! So happy for all 3 of you.",
+          "truncated": false
+        },
+        "statuses_count": 2609,
+        "time_zone": "Pacific Time (US & Canada)",
+        "url": null,
+        "utc_offset": -28800,
+        "verified": false,
+        "email": "test_email@test.com"
+      }
+      """;
+      final json = jsonDecode(resString);
 
       final user = User(json);
       expect(user is User, isTrue);
+      expect(user.id, 38895958);
+      expect(user.email, 'test_email@test.com');
+      expect(user.thumbnailImage,
+          'https://si0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG');
+      expect(user.name, 'Sean Cook');
+      expect(user.screenName, 'theSeanCook');
+      expect(user.createdAt, 'Sat May 09 17:58:22 +0000 2009');
+      expect(user.defaultProfile, isFalse);
+      expect(user.defaultProfileImage, isFalse);
+      expect(user.description,
+          'I taught your phone that thing you like.  The Mobile Partner Engineer @Twitter. ');
+      expect(user.favouritesCount, 588);
+      expect(user.followersCount, 10625);
+      expect(user.friendsCount, 1181);
+      expect(user.idStr, '38895958');
+      expect(user.listedCount, 190);
+      expect(user.location, 'San Francisco');
+      expect(user.protected, isFalse);
+      expect(user.statusesCount, 2609);
+      expect(user.url, isNull);
+      expect(user.verified, isFalse);
     });
 
     test('email is null', () {
-      final json = {
-        'email': null,
-        'profile_image_url_https': 'https://test.com/',
-        'name': 'test_name',
-        'screen_name': 'test_screen_name',
-      };
+      final resString = """
+      {
+        "contributors_enabled": true,
+        "created_at": "Sat May 09 17:58:22 +0000 2009",
+        "default_profile": false,
+        "default_profile_image": false,
+        "description": "I taught your phone that thing you like.  The Mobile Partner Engineer @Twitter. ",
+        "favourites_count": 588,
+        "followers_count": 10625,
+        "following": null,
+        "friends_count": 1181,
+        "id": 38895958,
+        "id_str": "38895958",
+        "listed_count": 190,
+        "location": "San Francisco",
+        "name": "Sean Cook",
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG",
+        "protected": false,
+        "screen_name": "theSeanCook",
+        "statuses_count": 2609,
+        "url": null,
+        "verified": false
+      }
+      """;
+      final json = jsonDecode(resString);
 
       final user = User(json);
       expect(user is User, isTrue);
+      expect(user.email, isNull);
+    });
+
+    test('description is null', () {
+      final resString = """
+      {
+        "contributors_enabled": true,
+        "created_at": "Sat May 09 17:58:22 +0000 2009",
+        "default_profile": false,
+        "default_profile_image": false,
+        "description": null,
+        "favourites_count": 588,
+        "followers_count": 10625,
+        "following": null,
+        "friends_count": 1181,
+        "id": 38895958,
+        "id_str": "38895958",
+        "listed_count": 190,
+        "location": "San Francisco",
+        "name": "Sean Cook",
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG",
+        "protected": false,
+        "screen_name": "theSeanCook",
+        "statuses_count": 2609,
+        "url": null,
+        "verified": false
+      }
+      """;
+      final json = jsonDecode(resString);
+
+      final user = User(json);
+      expect(user is User, isTrue);
+      expect(user.description, isNull);
+    });
+
+    test('location is null', () {
+      final resString = """
+      {
+        "contributors_enabled": true,
+        "created_at": "Sat May 09 17:58:22 +0000 2009",
+        "default_profile": false,
+        "default_profile_image": false,
+        "description": null,
+        "favourites_count": 588,
+        "followers_count": 10625,
+        "following": null,
+        "friends_count": 1181,
+        "id": 38895958,
+        "id_str": "38895958",
+        "listed_count": 190,
+        "location": null,
+        "name": "Sean Cook",
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG",
+        "protected": false,
+        "screen_name": "theSeanCook",
+        "statuses_count": 2609,
+        "url": null,
+        "verified": false
+      }
+      """;
+      final json = jsonDecode(resString);
+
+      final user = User(json);
+      expect(user is User, isTrue);
+      expect(user.location, isNull);
+    });
+
+    test('url is null', () {
+      final resString = """
+      {
+        "contributors_enabled": true,
+        "created_at": "Sat May 09 17:58:22 +0000 2009",
+        "default_profile": false,
+        "default_profile_image": false,
+        "description": null,
+        "favourites_count": 588,
+        "followers_count": 10625,
+        "following": null,
+        "friends_count": 1181,
+        "id": 38895958,
+        "id_str": "38895958",
+        "listed_count": 190,
+        "location": null,
+        "name": "Sean Cook",
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/1751506047/dead_sexy_normal.JPG",
+        "protected": false,
+        "screen_name": "theSeanCook",
+        "statuses_count": 2609,
+        "url": null,
+        "verified": false
+      }
+      """;
+      final json = jsonDecode(resString);
+
+      final user = User(json);
+      expect(user is User, isTrue);
+      expect(user.url, isNull);
     });
   });
 }
